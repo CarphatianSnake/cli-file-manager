0c0240da019bbb09b2ffc186fc62acd3f9c9d08e
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-31 19:22:57 +0200

    feat: implement showing of location on start and after each input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import { stdin as input, stdout as output } from 'node:process';
 import { createInterface } from 'node:readline';
+import { homedir } from 'node:os';
 import { getUsername } from './getUsername.js';
 
 const fileManager = () => {
+  let location = homedir();
   const username = getUsername();
   console.log(`Welcome to the File Manager, ${username}!`);
 
   const readline = createInterface({
     input,
     output,
-    prompt: 'Enter your command:\n',
+    prompt: `\nYou are currently in ${location}\n`,
   });
 
   readline.prompt();
@@ -20,7 +22,7 @@ const fileManager = () => {
     }
     readline.prompt();
   }).on('close', () => {
-    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
     process.exit(0);
   })
 };
